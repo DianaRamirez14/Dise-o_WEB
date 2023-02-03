5c9ab86d26df49d7302d9d57b05d4ac417dabb12
DianaRamirez <115669360+DianaRamirez14@users.noreply.github.com>
2023-02-03 22:53:21 +0100

    Primera versión

---
+++ 5.Formularios/main.js
@@ -0,0 +1,7 @@
+'use strict'
+//Variables o contenedores: Se usan para guardar un valor, es un espacio de memoria. Variable: Espacio de memoria que puede cambiar en el tiempo de ejecucion de memoria; Constante: Espacio de memoria que no cambia en el tiempo de ejecucion de memoria
+var numero1 = 13; //variable, caracter global
+let numero2 = 25; //caracter local
+const IVA = 19.1; //constante
+
+//console.log(numero1);
